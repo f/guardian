662e85a89d06f2f7c17657df9597fdc31ed695d6
Marcelo Boeira <me@marceloboeira.com>
2016-06-18 15:47:15 -0300

    Add --version argument

--- src/guardian.cr
+++ src/guardian.cr
@@ -6,6 +6,11 @@ options_parser = OptionParser.new do |options|
   options.on "init", "--init", "Generates the .guardian.yml file" do
     init = true
   end
+
+  options.on "-v", "--version", "Shows the version" do
+    puts "Guardian (#{Guardian::VERSION})"
+    exit
+  end
 end
 
 options_parser.parse(ARGV.clone)
--- src/guardian/version.cr
+++ src/guardian/version.cr
@@ -1,3 +1,3 @@
-module Warcher
+module Guardian
   VERSION = "0.1.0"
 end
