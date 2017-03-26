ff6b44e359218710267804f7eb3a136806b6e3e0
Isaac Sloan <isaac@isaacsloan.com>
2017-03-25 22:40:19 -0600

    fixed error with new version of crystal where parse options require - before them

--- src/guardian.cr
+++ src/guardian.cr
@@ -1,18 +1,23 @@
 require "./guardian/*"
 require "option_parser"
 
-options_parser = OptionParser.new do |options|
-  options.on "init", "Generates the .guardian.yml file" do
-    Guardian::Generator.new.generate
-    exit
-  end
+case ARGV[0]?
+when "init"
+  puts "\"init\" has been deprecated please use: -i or --init"
+  Guardian::Generator.new.generate
+  exit
+else
+  OptionParser.parse! do |options|
+    options.on "-i", "--init", "Generates the .guardian.yml file" do
+      Guardian::Generator.new.generate
+      exit
+    end
 
-  options.on "-v", "--version", "Shows the version" do
-    puts "Guardian (#{Guardian::VERSION})"
-    exit
+    options.on "-v", "--version", "Shows the version" do
+      puts "Guardian (#{Guardian::VERSION})"
+      exit
+    end
   end
 end
-
-options_parser.parse(ARGV.clone)
 
 Guardian::Watcher.new
