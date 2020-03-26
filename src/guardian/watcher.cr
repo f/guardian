d351ed5bbfbec8b873872192c13a522af73edcf9
wout <wout@mick-wout.com>
2020-03-26 08:44:48 +0000

    Using cyan for file name output.

--- src/guardian/watcher.cr
+++ src/guardian/watcher.cr
@@ -78,7 +78,7 @@ module Guardian
     def run_tasks(file)
       @runners[file].each do |command|
         command = command.gsub(/%file%/, file)
-        puts "#{"$".colorize(:dark_gray)} #{command.colorize(:red)}"
+        puts "#{"$".colorize(:dark_gray)} #{command.colorize(:cyan)}"
         output = `#{command}`
         output.lines.each do |line|
           puts "  #{line.gsub(/\n$/, "").colorize(:dark_gray)}"
